230d6a1f34ad5b4e9648e31f4c5eece1e41543eb
DC-RSingh <raje.singh@dcmail.ca>
2020-09-19 23:50:12 -0400

    Added test code in main

--- OOP3200-Lab1-WorkTicket/main.cpp
+++ OOP3200-Lab1-WorkTicket/main.cpp
@@ -4,13 +4,54 @@
  *		- 
  *
  *	@authors	Raje Singh & Angus Wai
- *	@studentIDs 100776793 & 
+ *	@studentIDs 100776793 & 100719558
+ *	@date		September 19, 2020
 */
 
 #include <iostream>
+//#include <vector>
+#include "WorkTicket.h"
 
 int main()
 {
-	
+	const int NUM_OF_TICKETS = 3;
+	//std::vector<WorkTicket> tickets;
+	WorkTicket tickets[NUM_OF_TICKETS];
+	int number;
+	string clientId;
+	int day;
+	int month;
+	int year;
+	string desc;
+
+	cout << "Please enter the details for the work ticket.\n";
+
+	for (int i = 0; i < NUM_OF_TICKETS; i ++)
+	{
+		do
+		{
+			cout << "\nCurrently inputting details for work ticket: " << i + 1 << endl;
+			cout << "\nTicket Number: ";
+			cin >> number;
+			cout << "Client ID: ";
+			cin >> clientId;
+			cout << "Day of the ticket: ";
+			cin >> day;
+			cout << "Month of the ticket: ";
+			cin >> month;
+			cout << "Year of the ticket: ";
+			cin >> year;
+			cout << "Description: ";
+			cin >> desc;
+
+			tickets[i].SetWorkTicket(number, clientId, day, month, year, desc);
+		} while (!tickets[i].IsValid());
+	}
+
+	for (int i = 0; i < NUM_OF_TICKETS; i++) cout << "\n\n--------------------\n       OUTPUT\n--------------------" << tickets[i].ShowWorkTicket();
+
+	/*WorkTicket test(-1, "44lsa", 19, 9, 2020, "This is a test ticket.");
+	cout << test.ShowWorkTicket();*/
+
 	return 0;
 }
